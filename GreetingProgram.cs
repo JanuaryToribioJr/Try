6a13efc4ece71756a2bebbe563b21aab48636902
JanuaryToribioJr <toribiojanuaryjr@gmail.com>
2024-08-20 11:59:21 +0800

    Add files via upload

---
+++ GreetingProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+    namespace GreetingApp
+{
+    class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int numOfCourse = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book: ");
+            double bookPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nName: " + name);
+            Console.WriteLine("Total enrolled courses: " + numOfCourse);
+            Console.WriteLine("Price of my favorite book: " + bookPrice);
+
+        }
+    }
+} //by January Toribio Jr.
